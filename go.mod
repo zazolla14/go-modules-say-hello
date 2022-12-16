6f662a7f722e341155333416ca062da0e7832f2e
azola <azola@gmail.com>
2022-12-17 00:58:16 +0700

    commit major changes

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zazolla14/go-modules-say-hello.git
+module github.com/zazolla14/go-modules-say-hello/v2
 
 go 1.19
--- say-hello.go
+++ say-hello.go
@@ -1,5 +1,16 @@
 package gomodulessayhello
 
-func SayHello() string {
-	return "Hello World Yeah"
+// v0.0.1
+// func SayHello() string {
+// 	return "Hello World Yeah"
+// }
+
+// v0.0.2
+// func SayHello() string {
+// 	return "Hello World Yeah"
+// }
+
+// v2.0.0
+func SayHello(name string) string {
+	return "Hello " + name
 }
